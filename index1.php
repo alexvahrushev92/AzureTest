40568342ca647fe1b0f4e8f9b4e01cd81a4cb9c5
1205982 <1205982@rgu.ac.uk>
2015-10-19 15:28:48 +0100

    hello world

--- index1.php
+++ index1.php
@@ -4,5 +4,21 @@
     </head>
 <body>
     <p><?php echo "Hello World";?></p>
+    <?php//variable
+    $myage=20;
+    $specs=16;
+    $mugs=18;
+    $sausage=21;
+
+    if $myage >= $specs;
+    echo "You're old enough to buy specs";
+    elseif $myage >= $mugs;
+    echo "You're old enough to buy mugs";
+    elseif $myage >= $sausage;
+    echo "You're old enough to buy sausage roll";
+    else
+        echo "you are too young"
+
+?>
 </body>
 </html>
