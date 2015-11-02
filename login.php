dce896a9a588ccac15f3c3f8d2c0b7b160b01e2a
1205982 <1205982@rgu.ac.uk>
2015-11-02 13:46:58 +0000

    hello world

--- login.php
+++ login.php
@@ -1,32 +1,4 @@
-<?php
-/* valid username and password */
-$user = 'johndoe';
-$pass = 'password';
 
-if (isset($_POST['username']) && isset($_POST['password')) {
-
-    if (($_POST['username'] == $user) && ($_POST['password'] == $pass)) {
-
-        if (isset($_POST['rememberme'])) {
-            /* Set cookie to last 1 year */
-            setcookie('username', $_POST['username'], time()+60*60*24*365, 'http://microsoftstillsucks.azurewebsites.net/login.php');
-            setcookie('password', md5($_POST['password']), time()+60*60*24*365, 'http://microsoftstillsucks.azurewebsites.net/login.php');
-
-        } else {
-            /* Cookie expires when browser closes */
-            setcookie('username', $_POST['username'], false, 'http://microsoftstillsucks.azurewebsites.net/login.php');
-            setcookie('password', md5($_POST['password']), false, 'http://microsoftstillsucks.azurewebsites.net/login.php');
-        }
-        header('Location: index.php');
-
-    } else {
-        echo 'Username/Password Invalid';
-    }
-
-} else {
-    echo 'You must supply a username and password.';
-}
-?>
 <html>
 <head>
     <title>User Login</title>
